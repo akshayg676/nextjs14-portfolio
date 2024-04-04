2f0afbdf004e050c9003cf84b871f4cde1e6b267
akshayg676 <akshayg676@gmail.com>
2024-04-04 18:36:19 +0530

    update not found component

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,3 +1,38 @@
-export default function notFound() {
-  return <div>not-found</div>;
-}
+import Link from "next/link";
+import styles from "../styles/ErrorPage.module.css";
+import { Modernist } from "./layout";
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>
+        Oops ! <span>404</span>
+      </p>
+      <code className={styles.code}>
+        <span>this_page</span>.<em>not_found</em> = true;
+      </code>
+      <code className={styles.code}>
+        <span>if</span> (<b>you_spelt_it_wrong</b>) {"{"}
+        <span>try_again()</span>;{"}"}
+      </code>
+      <code className={styles.code}>
+        <span>
+          else if (<b>we_screwed_up</b>)
+        </span>
+        &nbsp;
+        {"{"}
+        <em>alert</em>(<i>{`"We're really sorry about that."`}</i>);&nbsp;
+        <span>window</span>.<em>location</em> = home;{"}"}
+      </code>
+      <center>
+        <Link href="/" className={styles.redirect}>
+          <button className={`${styles.returnBtn} ${Modernist.className}`}>
+            Go Home
+          </button>
+        </Link>
+      </center>
+    </div>
+  );
+};
+
+export default NotFound;
